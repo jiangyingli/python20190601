612353439b7f04d6022d0c59b21c5f140674ec76
姜英利 <jiangyingli>
2019-06-29 20:36:25 +0800

    30日备课

---
+++ 姜英利/class630/sort.py
@@ -0,0 +1,38 @@
+import sys
+
+A = [64, 25, 12, 22, 11]
+
+for i in range(len(A)):
+
+    min_idx = i
+    for j in range(i + 1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+print("排序后的数组：")
+for i in range(len(A)):
+    print("%d" % A[i]),
+
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    # 遍历所有数组元素
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print("排序后的数组:")
+for i in range(len(arr)):
+    print("%d" % arr[i]),
