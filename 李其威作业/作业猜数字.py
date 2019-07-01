b56ef0f25d7c235fd34d3541fbd315ab953b06ba
liqiwei <1326944623@qq.com>
2019-07-01 14:05:11 +0800

    1

--- 李其威作业/作业猜数字.py
+++ 李其威作业/作业猜数字.py
@@ -1,10 +1,11 @@
 import random
 rand =random.randint(1,10)
 i=0
-while(i<2):
+while(i<3):
     i = i + 1
-    a = int(input("你猜是几\n"))
-    if(i<2):
+    if(i<3):
+        a = int(input("你猜是几\n"))
+
 
 
         if (a>rand):
@@ -16,7 +17,7 @@ while(i<2):
         elif(a==rand):
             print("真聪明猜对了")
         break
-    elif(i==2):
+    elif(i==3):
         if(a!=rand):
             print("游戏结束再见")
             break
