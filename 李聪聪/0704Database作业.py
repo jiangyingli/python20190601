11ccb5625c95c7f7a1895cbe336c749895f0ed8f
李聪聪 <1328550072@qq.com>
2019-07-05 16:55:46 +0800

    测试文件

--- 李聪聪/0704Database作业.py
+++ 李聪聪/0704Database作业.py
@@ -1,28 +1,28 @@
+# 1，做一个界面，1，新增图书    2，查看已有图书     3，查看特定图书（模糊查询）    4，修改图书信息
+
 import mysql.connector
+class mysqlconn:
+        cursor=None
+        mydb=None
+        def __init__(self):   #下划线，构造方法，创建对象自动调用一次
+                mydb = mysql.connector.connect(
+                        host="localhost",
+                        user="root",
+                        passwd="mysql",
+                        database="lcccccc",
+                        port="3308"
+                )
+                cursor = mydb.cursor()
 
-def update(sql,val=[]):
-        mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                passwd="mysql",
-                database="lcccccc",
-                port="3308"
-        )
-        cursor = mydb.cursor()
-        cursor.execute(sql, val)
-        mydb.commit()
+        def update(self,sql,val=[]):
 
-def query(sql,val=[]):
-        mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                passwd="mysql",
-                database="lcccccc",
-                port="3308"
-        )
-        cursor = mydb.cursor()
-        cursor.execute(sql, val)
-        return cursor.fetchall()
+                self.cursor.execute(sql, val)
+                self.mydb.commit()
+
+        def query(self,sql,val=[]):
+
+                self.cursor.execute(sql, val)
+                return self.cursor.fetchall()
 
 def fun1():
         code=input("输入code")
@@ -32,16 +32,16 @@ def fun1():
         pubdate=input("输入pubdate")
         price=input("输入price")
         val=[code,name,author,publish,pubdate,price]
-        update("insert into book (b_code,b_name,b_author,b_publish,b_pubdate,b_price) values(%s,%s,%s,%s,%s,%s)",val)
+        mysqlconn.update("insert into book (b_code,b_name,b_author,b_publish,b_pubdate,b_price) values(%s,%s,%s,%s,%s,%s)",val)
 
 def fun2():
-        rows = query("select * from book ")
+        rows = mysqlconn.query("select * from book ")
         for i in range(len(rows)):
                 print(rows[i])
 
 def fun3():
         val=[input("输入模糊的书名")]
-        rows =query("select * from book where b_name like '%'%s'%'",val)
+        rows =mysqlconn.query("select * from book where b_name like '%'%s'%'",val)
         for i in range(len(rows)):
                 print(rows[i])
 
@@ -49,32 +49,32 @@ def fun4():
         code=input("欲修改书的编号")
         name=input("修改书名为")
         val=[name,code]
-        update("update book set b_name=%s where b_code=%s",val)
+        mysqlconn.update("update book set b_name=%s where b_code=%s",val)
 
 def fun5():
         code = input("欲修改书的编号")
         author = input("修改作者为")
         val = [author, code]
-        update("update book set b_author=%s where b_code=%s", val)
+        mysqlconn.update("update book set b_author=%s where b_code=%s", val)
 
 def fun6():
         code = input("欲修改书的编号")
         publish = input("修改出版社为")
         val = [publish, code]
-        update("update book set b_publish=%s where b_code=%s", val)
+        mysqlconn.update("update book set b_publish=%s where b_code=%s", val)
 
 def fun7():
         code = input("欲修改书的编号")
         pubdate = input("修改出版日期为")
         val = [pubdate, code]
-        update("update book set b_pubdate=%s where b_code=%s", val)
+        mysqlconn.update("update book set b_pubdate=%s where b_code=%s", val)
 
 
 def fun8():
         code = input("欲修改书的编号")
         price = input("修改价格为")
         val = [price, code]
-        update("update book set b_price=%s where b_code=%s", val)
+        mysqlconn.update("update book set b_price=%s where b_code=%s", val)
 
 cmd=int(input("请输入指令：1.新增图书 2.查看已有图书 3.查看特定图书（模糊查询）4.修改图书信息"))
 if(cmd==1):
@@ -96,3 +96,6 @@ elif(cmd==4):
                 fun7()
         elif (cmd1 == 6):
                 fun8()
+
+
+label=["","","","","",""]
