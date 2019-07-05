c0d87f707c4811705596e79d44d897c967f5121c
DREAM <jdfzfjc@.163.com>
2019-07-05 10:18:00 +0800

    7.1

---
+++ 冯家琛/class7月4课堂.py
@@ -0,0 +1,37 @@
+import mysql.connector
+def query( sql , val):
+    mydb = mysql.connector.connect(
+        host="localhost",  # 设置数据库IP地址，localhost表示本机 127.0.0.1 表示本机
+        user="root",  # 连接MySQL数据库的账号，root是最高管理员
+        passwd="mysql",
+        database="jintian",  # 连接的数据库实例
+        port="3308"  # 端口
+    )
+    cursor = mydb.cursor()
+    cursor.execute(sql,val)
+    return cursor.fetchall()
+
+
+def add( sql , val):
+    mydb = mysql.connector.connect(
+        host="localhost",  # 设置数据库IP地址，localhost表示本机 127.0.0.1 表示本机
+        user="root",  # 连接MySQL数据库的账号，root是最高管理员
+        passwd="mysql",
+        database="jintian",  # 连接的数据库实例
+        port="3308"  # 端口
+    )
+    cursor = mydb.cursor()
+    cursor.execute(sql,val)
+    mydb.commit()
+
+def update (sql,val):
+    mydb = mysql.connector.connect(
+        host="localhost",  # 设置数据库IP地址，localhost表示本机 127.0.0.1 表示本机
+        user="root",  # 连接MySQL数据库的账号，root是最高管理员
+        passwd="mysql",
+        database="jintian",  # 连接的数据库实例
+        port="3308"  # 端口
+    )
+    cursor = mydb.cursor()
+    cursor.execute(sql, val)
+    mydb.commit()
