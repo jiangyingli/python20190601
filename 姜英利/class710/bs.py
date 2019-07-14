92a372f7f69b06f60a1298239aa1013a5122ab38
jyl <jiangyingli@126.com>
2019-07-14 15:50:27 +0800

    测试文件

--- 姜英利/class710/bs.py
+++ 姜英利/class710/bs.py
@@ -13,15 +13,20 @@ html = """
         <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
         and they lived at the bottom of a well.
     </p>
-    <p class="story">...</p>
+    <p class="story">
+        <span>你好</span>
+    </p>
     </body>
 </html>
 """
 soup = BeautifulSoup(html, "html.parser")
+tag = soup.find_all("p",class_="story")[-1]
+print(tag.get_text(strip="True"))
+
 # 找到第一个a标签, 返回对象
-tag1 = soup.find(name='a')
+# tag1 = soup.find(name='a')
 # 找到所有的a标签，返回对象
-tag2 = soup.find_all(name='a')
+# tag2 = soup.find_all(name='a')
 # 找到id＝link2的标签，返回对象,css选择器语法
-tag3 = soup.select('#link2')
-print(tag3)
+# tag3 = soup.soupselect('#link2')
+# print(tag3)
